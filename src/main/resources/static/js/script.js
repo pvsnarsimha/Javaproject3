c94e8ffe9e05bf3edecd8d898b909dc6ea06a871
pvsnarsimha <114327962+pvsnarsimha@users.noreply.github.com>
2025-09-10 06:34:42 +0530

    Delete src directory

--- src/main/resources/static/js/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.quick-actions button').forEach(button => {
-    button.addEventListener('click', function() {
-        const megaMenu = this.nextElementSibling;
-        megaMenu.style.display = megaMenu.style.display === 'block' ? 'none' : 'block';
-    });
-});
